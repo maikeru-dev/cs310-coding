31cfcadfe93eeb920598190d944854ab9cca1478
maikeru-dev <dprofilo7@gmail.com>
2025-03-01 02:23:53 +0000

    [fix] minimax

--- week05/main.py
+++ week05/main.py
@@ -6,36 +6,37 @@ from time import sleep, perf_counter
 from types import FunctionType
 
 
-class Turn(Enum):
-    MAX = 2
-    MIN = 1
-    END = 0
+def delItem(tup, index):
+    return tup[:index] + tup[index + 1 :]
+
+
+def setItem(tup, index, value):
+    return tup[:index] + (value,) + tup[index + 1 :]
 
 
 def takeN(piles, take, index):
-    newPile = piles.copy()
     if piles[index] - take == 0:
-        del newPile[index]
-        return sorted(newPile)
-    newPile[index] -= take
-    return sorted(newPile)
+        return delItem(piles, index)
+    return setItem(piles, index, piles[index] - take)
 
 
-def successors(piles: list[int]):
+@lru_cache(None)
+def successors(piles):
     nextPiles = []
     for index in range(len(piles)):
         pile = piles[index]
-        for take in range(1, pile + 1):
+        for take in range(1, min(pile + 1, 4)):
             successor = takeN(piles, take, index)
-            if successor is not None and successor not in nextPiles:
-                nextPiles.append(successor)
+            nextPiles.append(successor)
     return nextPiles
 
 
 def maxChoice(piles):
     maxEval = float("-inf")
-    for s in successors(piles):
-        eval = minimax_value((s, 1))
+    succs = successors(tuple(piles))
+    # print(succs)
+    for s in succs:
+        eval = minimax_value((s, 2))
         maxEval = max(maxEval, eval)
 
     return maxEval
@@ -43,18 +44,12 @@ def maxChoice(piles):
 
 def minChoice(piles):
     minEval = float("inf")
-    for s in successors(piles):
-        eval = minimax_value((s, 2))
+    succs = successors(tuple(piles))
+    # print(succs)
+    for s in succs:
+        eval = minimax_value((s, 1))
         minEval = min(minEval, eval)
     return minEval
-
-
-def measurePerf(fn: FunctionType, *args):
-    t1_start = perf_counter()
-    res = fn(*args)
-    t1_stop = perf_counter()
-    print("Time measured: ", t1_stop - t1_start)
-    return res
 
 
 def minimax_value(state):
@@ -65,10 +60,16 @@ def minimax_value(state):
 
     if turn == 1:
         return maxChoice(piles)
-    elif turn == 2:
-        return minChoice(piles)
     else:
-        return 0
+        return minChoice(piles)
+
+
+def measurePerf(fn: FunctionType, *args):
+    t1_start = perf_counter()
+    res = fn(*args)
+    t1_stop = perf_counter()
+    print("Time measured: ", t1_stop - t1_start)
+    return res
 
 
 # print(minimax_value(State([2, 1], Turn.MAX)))
